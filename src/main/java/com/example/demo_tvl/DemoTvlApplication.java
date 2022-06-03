071726eb81bc58f36302327e7e845bf6f5ca3124
thamminh <thamminh512@gmail.com>
2022-06-03 18:50:18 +0700

    second

--- src/main/java/com/example/demo_tvl/DemoTvlApplication.java
+++ src/main/java/com/example/demo_tvl/DemoTvlApplication.java
@@ -2,10 +2,16 @@ package com.example.demo_tvl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoTvlApplication {
-
+	@GetMapping("/message")
+	public String message(){
+		return "Ola hahaha";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoTvlApplication.class, args);
 	}
