ecda8bc4fa2ee97a7822158786b35b52b9269252
thamminh <thamminh512@gmail.com>
2022-06-03 14:01:10 +0700

    first commit

---
+++ src/test/java/com/example/demo_tvl/DemoTvlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_tvl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTvlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
